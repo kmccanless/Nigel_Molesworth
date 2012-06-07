19080dbef425d22de0811ec4d8d829cdff53dcbb
Keith McCanless <common@mccanless.us>
2012-06-07 08:29:56 -0400

    removed the merge makeup from the files that git added.  should have done it before the last commit but still learning.

--- OptimizationDashboard/OptimizationDashboard/Scripts/data-service.js
+++ OptimizationDashboard/OptimizationDashboard/Scripts/data-service.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿var dataService = {
 	self: this,
 	ready: function (callback) { callback },
@@ -37,44 +36,4 @@
 					callback(data);
 				})
 	}
-=======
-﻿var dataService = {
-    self: this,
-    ready: function (callback) { callback },
-    values: undefined,
-    setValues: function (values) {
-        self.values = values;
-    },
-    defineMockService: function (json) {
-        amplify.request.define("MockService", function (settings) {
-            settings.success(json);
-        })
-    },
-    defineDataService: function () {
-        amplify.request.define("DataService", "ajax", {
-            url: "/DataService.aspx?userName={userName}&location={location}",
-            type: "GET"
-        })
-    },
-    callMockService: function (callback) {
-        console.log("calling mock service");
-        amplify.request({
-            resourceId: "MockService",
-            success: function (data) {
-                callback(data);
-            },
-            error: function (message, level) {
-                alert(message + " " + level);
-            }
-        })
-    },
-    callDataService: function (callback) {
-        console.log("calling data service");
-        amplify.request("DataService",
-				self.values,
-				function (data) {
-				    callback(data);
-				})
-    }
->>>>>>> 04ed25548d249746e16ddd588b7265fe98a3b399
 }
