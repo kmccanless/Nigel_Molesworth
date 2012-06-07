0a6b9bd68b1468b16efc20e0502172090d314d1e
Keith McCanless <common@mccanless.us>
2012-06-07 14:50:31 -0400

    more tests

--- OptimizationDashboard/OptimizationDashboard/Tests/dashboard-tests.js
+++ OptimizationDashboard/OptimizationDashboard/Tests/dashboard-tests.js
@@ -9,47 +9,47 @@
 //    }
 });
 
-//test("Page has content", function () {
-//    ok(S("body *").size(), "Page Loaded without elements")
-//});
+test("Page has content", function () {
+    ok(S("body *").size(), "Page Loaded without elements")
+});
 
-//test("Displays correct user name", function () {
-//    equal(S('#userName').text(), "Joe User", "User name is correct")
-//});
+test("Displays correct user name", function () {
+    equal(S('#userName').text(), "Joe User", "User name is correct")
+});
 
-//test("Displays correct role", function () {
-//    equal(S('#role').text(), "some role", "Role is correct")
-//});
+test("Displays correct role", function () {
+    equal(S('#role').text(), "some role", "Role is correct")
+});
 
-//test("Location defaults to All", function () {
-//    equal(S('#selLocations option:selected').val(), "All", "Dropdown did defaulted to All");
-//});
+test("Location defaults to All", function () {
+    equal(S('#selLocations option:selected').val(), "All", "Dropdown did defaulted to All");
+});
 
-//test("Location drop down contains correct locations", function () {
-//    var correct;
-//    var options = S('#selLocations').children()(function () {
-//        correct = correct + S(this).val();
-//    });
-//    equal(correct, "1356All", "Dropdown has correct locations");
-//});
+test("Location drop down contains correct locations", function () {
+    var correct;
+    var options = S('#selLocations').children()(function () {
+        correct = correct + S(this).val();
+    });
+    equal(correct, "1356All", "Dropdown has correct locations");
+});
 
-//test("Location defaults to All", function () {
-//    equal(S('#selLocations option:selected').val(), "All", "Dropdown did defaulted to All");
-//});
+test("Location defaults to All", function () {
+    equal(S('#selLocations option:selected').val(), "All", "Dropdown did defaulted to All");
+});
 
-//test("Two workspaces present", function () {
-//    equal(S('.workspace').size(), 2, "Workspaces present");
-//});
+test("Two workspaces present", function () {
+    equal(S('.workspace').size(), 2, "Workspaces present");
+});
 
-//test("5 KPIs under Top workspace", function () {
-//    var kpis = S('#workspace_1  .kpi').size();
-//    equal(kpis, 5, "Correct number of KPI");
-//});
+test("5 KPIs under Top workspace", function () {
+    var kpis = S('#workspace_1  .kpi').size();
+    equal(kpis, 5, "Correct number of KPI");
+});
 
-//test("5 KPIs under Bottom workspace", function () {
-//    var kpis = S('#workspace_2  .kpi').size();
-//    equal(kpis, 5, "Correct number of KPI");
-//});
+test("5 KPIs under Bottom workspace", function () {
+    var kpis = S('#workspace_2  .kpi').size();
+    equal(kpis, 5, "Correct number of KPI");
+});
 
 test("Data correct in 1st KPI of top row", function () {
     equal(S('#id_1_1 #kpi_title').text(), "First Load", "Title Test");
