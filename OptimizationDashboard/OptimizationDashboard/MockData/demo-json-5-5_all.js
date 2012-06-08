7f8c47074a7f081feafc309b49380f0be3d33a18
Joe Hiestand <jhiestand@commandalkon.com>
2012-06-08 07:49:07 -0400

    added display start and end times

--- OptimizationDashboard/OptimizationDashboard/MockData/demo-json-5-5_all.js
+++ OptimizationDashboard/OptimizationDashboard/MockData/demo-json-5-5_all.js
@@ -14,43 +14,58 @@ var KPI_JSON = {
 			KPIs: [
 				{
 					title: "FIRST LOAD",
-					type: "minutes",
+					type: "Minutes",
 					target: 370,
+					value: 885,
 					units: 25,
 					firstWarn: 740,
 					secondWarn: 1110,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                 },
                 {
                     title: "JOB WAIT",
-                    type: "minutes",
+                    type: "Minutes",
                     target: 416,
-                    units: 23.19,
+                    value: 835,
+					units: 23.19,
                     firstWarn: 624,
-                    secondWarn: 832
+                    secondWarn: 832,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                 },
                 {
                     title: "YARD TIME",
-                    type: "minutes",
+                    type: "Minutes",
                     target: 600,
-                    units: 21.5,
-                    firstWarn: 624,
-                    secondWarn: 832
+                    value: 860,
+					units: 21.5,
+                    firstWarn: 800,
+                    secondWarn: 1000,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                 },
                  {
                      title: "EOD YARD",
-                     type: "minutes",
+                     type: "Minutes",
                      target: 864,
-                     units: 21,
+                     value: 1125,
+					 units: 21,
                      firstWarn: 1146,
-                     secondWarn: 1428
+                     secondWarn: 1428,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                  },
                   {
                       title: "TOTAL COMPLIANCE",
-                      type: "percentage",
+                      type: "Percentage",
                       target: 207,
-                      units: 90,
+                      value: 181,
+					  units: 90,
                       firstWarn: 195.5,
-                      secondWarn: 184.0
+                      secondWarn: 184.0,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                   }
             ]
         },
@@ -59,43 +74,58 @@ var KPI_JSON = {
             KPIs: [
                 {
                     title: "LOCKED LOADS",
-                    type: "percentage",
+                    type: "Percentage",
                     target: 11.4,
-                    units: 10,
+                    value: 22.0,
+					units: 10,
                     firstWarn: 22.8,
-                    secondWarn: 34.2
+                    secondWarn: 34.2,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                 },
                 {
                     title: "STARTUP",
-                    type: "minutes",
+                    type: "Minutes",
                     target: 1660,
-                    units: 36,
+                    value: 2515,
+					units: 36,
                     firstWarn: 2350,
-                    secondWarn: 3040
+                    secondWarn: 3040,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                 },
                 {
                     title: "LOAD & MIX",
-                    type: "minutes",
+                    type: "Minutes",
                     target: 1041,
-                    units: 18,
+                    value: 1330,
+					units: 18,
                     firstWarn: 1366,
-                    secondWarn: 1625
+                    secondWarn: 1625,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                 },
                 {
                     title: "WASH OUT",
-                    type: "minutes",
+                    type: "Minutes",
                     target: 860,
-                    units: 41,
+                    value: 1745,
+					units: 41,
                     firstWarn: 1290,
-                    secondWarn: 1720
+                    secondWarn: 1720,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                 },
                 {
                     title: "STATUS BOX UPDATES",
-                    type: "percentage",
+                    type: "Percentage",
                     target: 218.5,
-                    units: 91,
+                    value: 210.0,
+					units: 91,
                     firstWarn: 207.0,
-                    secondWarn: 195.5
+                    secondWarn: 195.5,
+					displayStartTime: "8:30am",
+					displayEndTime: "5:30pm"
                 }
             ]
         }
